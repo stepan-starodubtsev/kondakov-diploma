d2beb9081b3db596c99ddc83ff6da5bb320be74f
Stepan Starodubtsev <stepan.starodubtsev@devops.ncia.nato.int>
2025-05-18 18:17:16 +0300

    temp backend security done

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const {userToDto} = require("../dtos/user.dto");
 require('dotenv').config();
 
 exports.register = async (req, res) => {
@@ -52,6 +53,24 @@ exports.login = async (req, res) => {
         });
     } catch (err) {
         console.error(err.message);
+        res.status(500).send('Server error');
+    }
+};
+
+exports.getMe = async (req, res) => {
+    try {
+        if (!req.user || !req.user.user || !req.user.user.id) {
+            return res.status(400).json({ message: 'User ID not found in token payload' });
+        }
+        const userId = req.user.user.id;
+        const user = await User.findByPk(userId);
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.json(userToDto(user));
+    } catch (err) {
+        console.error("GetMe error:", err.message);
         res.status(500).send('Server error');
     }
 };
